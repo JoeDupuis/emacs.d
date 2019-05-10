4dada48e301cb4a547af54f595525f765bd14466
Joe Dupuis <joe.dupuis@xylio.net>
2019-05-10 16:59:47 +0800

    Update

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -12,11 +12,17 @@
  '(beacon-color "#cc6666")
  '(blink-cursor-mode nil)
  '(column-number-mode t)
+ '(company-backends
+   (quote
+    (company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-dabbrev company-css)))
  '(compilation-message-face (quote default))
  '(custom-enabled-themes (quote (monokai-alt)))
  '(custom-safe-themes
    (quote
     ("d1ede12c09296a84d007ef121cd72061c2c6722fcb02cb50a77d9eae4138a3ff" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(dired-dwim-target t)
  '(dired-listing-switches "-alh")
  '(fancy-splash-image "")
  '(fci-rule-color "#373b41")
@@ -37,10 +43,10 @@
      ("#3C3D37" . 100))))
  '(magit-diff-use-overlays nil)
  '(menu-bar-mode nil)
- '(package-archive-priorities (quote (("elpa" . 5) ("melpa" . 3))))
+ '(package-archive-priorities (quote (("gnu" . 5) ("melpa" . 3))))
  '(package-selected-packages
    (quote
-    (flycheck yaml-mode rails-log-mode projectile-rails projectile highlight-indentation wgrep-ag goto-chg magit-gitflow buffer-move ag monokai-alt-theme monokai-theme use-package magit color-theme-sanityinc-tomorrow)))
+    (ledger-mode php-mode company web-mode exec-path-from-shell restclient minitest ruby-end electric-pair flx-ido markdown-mode csharp-mode flycheck yaml-mode rails-log-mode projectile-rails projectile highlight-indentation wgrep-ag goto-chg magit-gitflow buffer-move ag monokai-alt-theme monokai-theme use-package magit color-theme-sanityinc-tomorrow)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(scroll-bar-mode nil)
@@ -48,6 +54,7 @@
  '(size-indication-mode t)
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
+ '(use-dialog-box nil)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -70,6 +77,7 @@
      (340 . "#f0c674")
      (360 . "#b5bd68"))))
  '(vc-annotate-very-old-color nil)
+ '(web-mode-auto-close-style 1)
  '(weechat-color-list
    (quote
     (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 ;; (add-to-list 'package-archives
 ;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+'("melpa" . "https://melpa.org/packages/") t)
 
 
 
@@ -22,41 +22,89 @@
 
 (eval-when-compile
 
-  (require 'use-package))
+(require 'use-package))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(use-package exec-path-from-shell :ensure t
+
+)
+
+;; (exec-path-from-shell-initialize)
+;; (exec-path-from-shell-copy-env "XDG_CONFIG_HOME")
+;; (exec-path-from-shell-copy-env "XDG_DATA_HOME")
 
 (use-package magit :ensure t :defer t :bind ("C-x g" . magit))
 (use-package magit-gitflow  :ensure t :hook  (magit-mode . turn-on-magit-gitflow))
 (use-package color-theme-sanityinc-tomorrow :defer t :ensure t)
 (use-package monokai-theme :defer t :ensure t)
 (use-package monokai-alt-theme :defer t :ensure t
-  :config
-  (set-face-attribute 'mode-line           nil :foreground "black" :background "dark orange")
-  (set-face-attribute 'mode-line-buffer-id           nil :foreground "black")
-  (set-face-attribute 'mode-line-inactive           nil :foreground "orange" :background "#363631")
-    )
+:config
+(set-face-attribute 'mode-line           nil :foreground "black" :background "dark orange")
+(set-face-attribute 'mode-line-buffer-id           nil :foreground "black")
+(set-face-attribute 'mode-line-inactive           nil :foreground "orange" :background "#363631")
+)
 (use-package ido :config
-  (setq ido-everywhere t)
-  (setq ido-virtual-buffers t)
-  (ido-mode))
-(use-package ido-better-flex :load-path "vendor/ido-better-flex")
+(setq ido-everywhere t)
+(setq ido-virtual-buffers t)
+(put 'dired-do-copy 'ido 'dired-do-copy)
+(put 'dired-do-rename 'ido 'dired-do-rename)
+(setq ido-default-buffer-method 'samewindow)
+(ido-mode))
+
+
+;use-package ido-better-flex :load-path "vendor/ido-better-flex")
+(use-package flx-ido :ensure t)
 
 (use-package ag :ensure t :bind
-  ("C-c C-x C-f" . ag))
+("C-c C-x C-f" . ag))
 (use-package wgrep-ag :ensure t :defer t)
 
 (use-package goto-chg :ensure t :bind
-  ("C-." . goto-last-change)
-  ("C-," . goto-last-change-reverse))
+("C-." . goto-last-change)
+("C-," . goto-last-change-reverse))
 
 (use-package highlight-indentation :ensure t :defer t)
+(use-package php-mode :ensure t :defer t)
+(use-package csharp-mode :ensure t :defer t)
+(use-package ledger-mode :ensure t :defer t :mode "\\.ledger\\'")
+(use-package web-mode :ensure t :defer t :mode "\\.erb\\'" :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq-default indent-tabs-mode nil)
 
-(use-package projectile :ensure t :defer t)
-(use-package projectile-rails :ensure t :defer t)
+  )
+(use-package markdown-mode :ensure t :defer t)
+
+(use-package projectile
+  :ensure t
+  :init
+					;  (setq projectile-completion-system 'ivy)
+  (setq projectile-project-search-path '("~/Documents/"))
+
+  :config
+  (use-package projectile-rails :ensure t)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+  (projectile-rails-global-mode)
+
+(use-package smart-tabs-mode :ensure t )
+
+(use-package company :ensure t   :init (global-company-mode)
+)
+
+(electric-pair-mode 1)
+(use-package restclient :ensure t)
+(use-package ruby-end :ensure t :hook (ruby-mode . minitest-mode)
+  :bind (
+	 ("C-c C-c" . minitest-verify-single)
+	 ("C-c C-t" . minitest-verify)
+	 ("C-x C-t" . minitest-verify-all)
+	 ))
+(use-package minitest :ensure t)
+(use-package aggressive-indent :ensure t)
+(use-package dtrt-indent :ensure t)
 (use-package rails-log-mode :ensure t :defer t)
-(use-package yaml-mode :ensure t :defer t)
+(use-package yaml-mode :ensure t :defer t :mode "\\.sls\\'")
 (use-package flycheck :ensure t :defer t)
 (use-package goto-gem :ensure t :defer t)
 (use-package windmove
@@ -163,3 +211,21 @@
   (kill-ring-save (region-beginning) (region-end))
 )
 (global-set-key (kbd "M-k") 'duplicate-line)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Delete current buffer file
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
